cde16996d37b2e67c8feaa36e1c6025e8e0355d1
JWH <jwheneghan@hotmail.com>
2024-05-23 13:17:06 -0230

    Working Even Keegan. Description bugged

--- mods/example.lua
+++ mods/example.lua
@@ -4,19 +4,13 @@ local mod_version = "1.0"
 local mod_author = "fortress"
 
 local function jokerEffect(card, context)
-    if context.individual then
-        if context.cardarea == G.play then
-            if card.ability.name == 'Even Keegan' then
-                return {
-                    message = localize {
-                        type = 'variable',
-                        key = 'a_xmult',
-                        vars = {"hello!"}
-                    },
-                    mult = card.ability.extra.mult,
-                    card = card
-                }
-            end
+    if card.ability.name == 'Even Keegan' and context.individual and context.cardarea == G.play then
+        if context.other_card:get_id() <= 10 and context.other_card:get_id() >= 0 and context.other_card:get_id() % 2 ==
+            0 then
+            return {
+                x_mult = 2,
+                card = card
+            }
         end
     end
 end
@@ -41,7 +35,7 @@ table.insert(mods, {
         nil, -- internal effect description
         {
             extra = {
-                mult = 2
+                x_mult = 2
             }
         }, -- config
         {"{Even cards give", "{C:red}X2{} Mult"}, -- description text
@@ -64,3 +58,4 @@ table.insert(mods, {
         centerHook.removeJoker(self, "j_evenkeegan_fortress")
     end
 })
+
